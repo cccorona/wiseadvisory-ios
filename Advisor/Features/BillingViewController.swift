db47e61e61313f0631adcd29e0abcf97a8282123
Cesar Corona <itachi.upiita@gmail.com>
2020-11-17 00:16:00 -0600

    Keyboard flow was improved

--- Advisor/Features/BillingViewController.swift
+++ Advisor/Features/BillingViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
 class BillingViewController: UIViewController {
 
+    
+    @IBOutlet weak var socialTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var rfcTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var directionFiscalTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var cfdiTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var bankTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var clabeTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var locationTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var stateTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var cityTxtField: SkyFloatingLabelTextField!
+    @IBOutlet weak var postalCodeTxtField: SkyFloatingLabelTextField!
+    var activeTextField : UITextField? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -23,13 +37,40 @@ class BillingViewController: UIViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(dissmisKeyboard))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
+        socialTxtField.delegate = self
+        rfcTxtField.delegate = self
+        directionFiscalTxtField.delegate = self
+        cfdiTxtField.delegate = self
+        bankTxtField.delegate = self
+        clabeTxtField.delegate = self
+        locationTxtField.delegate = self
+        stateTxtField.delegate = self
+        cityTxtField.delegate = self
+        postalCodeTxtField.delegate = self
     }
 
     @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height  - NavigationMenuBaseController.tabBarHeight
-            }
+//        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+//            if self.view.frame.origin.y == 0 {
+//                self.view.frame.origin.y -= keyboardSize.height  - NavigationMenuBaseController.tabBarHeight
+//            }
+//        }
+//
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+          return
+        }
+
+        var shouldMoveViewUp = false
+        if let activeTextField = activeTextField {
+          let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
+          let topOfKeyboard = self.view.frame.height - keyboardSize.height + NavigationMenuBaseController.tabBarHeight
+          if bottomOfTextField > topOfKeyboard {
+            shouldMoveViewUp = true
+          }
+        }
+
+        if(shouldMoveViewUp) {
+          self.view.frame.origin.y = 0 - keyboardSize.height  +  NavigationMenuBaseController.tabBarHeight
         }
     }
 
@@ -43,4 +84,14 @@ class BillingViewController: UIViewController {
         self.view.endEditing(true)
     }
 
+}
+
+extension BillingViewController : UITextFieldDelegate {
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    self.activeTextField = textField
+  }
+
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    self.activeTextField = nil
+  }
 }
